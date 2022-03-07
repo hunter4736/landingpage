dd9eb3a4b9c6d6b3b5ec9673a82b94ef0247ee1a
Hunter Smith <smithrhunter@gmail.com>
2022-03-07 10:03:26 -0500

    creted responsiveness, nav links,  sticky header,  and active section in js.

--- js/app.js
+++ js/app.js
@@ -1,32 +1,79 @@
+/*  sticky header  */
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("nav-menu");
+    header.classList.toggle("sticky", window)
+  })
+
+/* create responsive mobile menu */  
 
 const navMenu = () => {
-  const mobilemenu = document.querySelector(".mobilemenu");
-  const nav = document.querySelector(".nav-links");
+    const mobilemenu = document.querySelector(".mobilemenu");
+    const nav = document.querySelector(".nav-links");
+    
+    mobilemenu.addEventListener('click', ()=> {
+      nav.classList.toggle('nav-links-active')
+    })
+}
+    
+    navMenu(); 
 
-  mobilemenu.addEventListener('click', ()=> {
-    nav.classList.toggle('nav-links-active')
-  })
+/*  creating nav list */
+
+const navLinks = document.getElementById('nav-links');
+const sec = document.querySelectorAll('section');
+
+Array.from(sec);
+
+let navNum = sec.length;
+
+function createLi () {
+    for (section of sec) {
+        secLink = section.getAttribute('id');
+        secName = section.getAttribute('data-nav');
+        liLink = document.createElement('li');
+        liLink.innerHTML = `<a class = 'nav-links' href = '#${secLink}'>${secName}</a> `;
+        navLinks.appendChild(liLink);
+    }
 }
 
-navMenu();
+/* find active section */
 
-
-var html="";
-
-function navbar(){
-  var links=[
-  ["Home", "home.html"],
-  ["shop", "shop.html"],
-  ["Our Story", "ourstory.html"]
-  ["Contact", "contact.html"]
-  ];
-  for (var i=0; i<links.length; i++)
-  {
-    var str=""+links[i];
-    str=str.split(',');
-    html=html+"<span class='link'><a onmouseover=\"this.className='onhover'"
-      onmouseout="this.className='onout'\" href='"+str[1]+"'>"+str[0]+"</a>";
-    document.getElementsByClassName('nav-links').innerHTML=html;
-  }
+function current(par) {
+    let secpar = par.getBoundingClientRect();
+    return (secpar.top >= 0 && secpar.top < 300); 
 }
-navbar(); 
+
+function activeSec() {
+    for (section of sec) {
+        if (current(section)) {
+            if (!section.classList.contains('body')) {
+
+            section.classList.add('body');    
+            }
+        }
+        else {
+            section.classList.remove('body')
+        }
+    }
+}
+createLi();
+document.addEventListener('scroll', activeSec);
+
+/* button that returns user to top of page */
+
+const sendItButton = document.getElementById('senditbtn');
+window.onscroll = function () { scroll()} ;
+function scroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        sendItButton.style.display = "block";
+
+    } else {
+        sendItButton.style.display = "none";
+    } 
+}
+
+function sendIt() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
